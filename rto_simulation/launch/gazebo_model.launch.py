1f995f758d40ba9992fa4ac55b5b89a730150761
Michael Salvatierra <salvmike0@gmail.com>
2026-06-10 16:57:05 -0600

    Swithing to Gazebo Fortress

---
+++ rto_simulation/launch/gazebo_model.launch.py
@@ -0,0 +1,95 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    pkg_name = "rto_simulation"
+
+    world_file = PathJoinSubstitution([
+        FindPackageShare(pkg_name),
+        "worlds",
+        "robotino_empty.sdf"
+    ])
+
+    robot_xacro = PathJoinSubstitution([
+        FindPackageShare("rto_description"),
+        "urdf",
+        "robots",
+        "rto-3.urdf.xacro"
+    ])
+
+    robot_description_content = Command([
+        "xacro ",
+        robot_xacro
+    ])
+
+    robot_description = {
+        "robot_description": ParameterValue(
+            robot_description_content,
+            value_type=str
+        )
+    }
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("ros_gz_sim"),
+                "launch",
+                "gz_sim.launch.py"
+            ])
+        ),
+        launch_arguments={
+            "gz_args": ["-r -v 4 ", world_file]
+        }.items()
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            robot_description,
+            {"use_sim_time": True}
+        ],
+        output="screen"
+    )
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "robotino",
+            "-topic", "robot_description",
+            "-x", "0.0",
+            "-y", "0.0",
+            "-z", "0.05"
+        ],
+        output="screen"
+    )
+
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/lidar_scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+            "/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist",
+        ],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+
+        TimerAction(
+            period=3.0,
+            actions=[spawn_robot]
+        ),
+        bridge,
+    ])
---
+++ rto_simulation/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'rto_simulation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.sdf') + glob('worlds/*.world')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mike',
+    maintainer_email='salvmike0@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
